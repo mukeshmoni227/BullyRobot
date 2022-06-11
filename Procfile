63536aba6081d27e019365374d8702eb6fa2aa97
cyberxron <ffidhack87@gmail.com>
2022-06-11 20:19:01 +0530

    BullyRobot

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m BullyRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Bully Robot
+:: Enables virtual env mode and then starts Fallen
+env\scripts\activate.bat && py -m FallenRobot
