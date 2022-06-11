35803e5852842a66f15c7280ebe207726ee14ed3
cyberxron <99018732+cyberxron@users.noreply.github.com>
2022-06-11 21:11:10 +0530

    Update gettime.py

--- BullyRobot/modules/gettime.py
+++ BullyRobot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from FallenRobot import TIME_API_KEY, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from BullyRobot import TIME_API_KEY, dispatcher
+from BullyRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
