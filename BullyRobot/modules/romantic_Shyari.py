d2e14d39063153d23fc0a5ca27c870bbcf7f0bd3
cyberxron <99018732+cyberxron@users.noreply.github.com>
2022-06-21 14:36:10 +0530

    Update romantic_Shyari.py

--- BullyRobot/modules/romantic_Shyari.py
+++ BullyRobot/modules/romantic_Shyari.py
@@ -1,7 +1,7 @@
 import random
 import asyncio
 from pyrogram import filters
-from FallenRobot import pbot as FallenRobot
+from BullyRobot import pbot as BullyRobot
 
 
 
@@ -34,7 +34,7 @@ ROMANTIC_STRINGS = [
     Coded by : @CoderX on telegram...
 """
 
-@FallenRobot.on_message(filters.command("shayari"))
+@BullyRobot.on_message(filters.command("shayari"))
 async def lel(bot, message):
     ran = random.choice(ROMANTIC_STRINGS)
     await bot.send_chat_action(message.chat.id, "typing")
